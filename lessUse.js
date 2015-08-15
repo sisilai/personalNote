c6d0d5d004fb28daac001a90f95cd363eef5d298
jxgzlss1 <773871685@qq.com>
2015-08-15 17:36:28 +0800

    转换为单独的正则表达式文件

--- lessUse.js
+++ lessUse.js
@@ -4,7 +4,4 @@
 var less = require('less');
 less.render('.class1 {width: 1+1;}',function(e,css){
     console.log(css);
-    var re = /he/;//最简单的正则表达式,将匹配he这个单词
-    var str = "he";
-    alert(re.test(str));//true
 });
