842ee31f81626852403bc986b9c1e1bef2f60ed0
sisilai <1093132971@qq.com>
2015-09-11 09:35:48 +0800

    express使用入门

---
+++ 20150910/app.js
@@ -0,0 +1,20 @@
+/**
+ * Created by sisilai on 2015/9/10.
+ */
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World!111111');
+});
+
+app.get('/s', function (req, res) {
+    res.send('Hello World!111111sss');
+});
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log('Example app listening at http://%s:%s', host, port);
+});
