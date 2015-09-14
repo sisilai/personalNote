4bc75f2c6063ed66723b27818a75a5fa1bb34196
sisilai <1093132971@qq.com>
2015-09-14 10:53:24 +0800

    express路由控制

--- 20150910/app.js
+++ 20150910/app.js
@@ -4,9 +4,28 @@
 var express = require('express');
 var app = express();
 
+//var app = require('express')();
+
 app.get('/', function (req, res) {
     res.send('Hello World!111111');
 });
+
+app.get('/example/b', function (req, res, next) {
+    console.log('response will be sent by the next function ...')
+    next()
+}, function (req, res) {
+    res.send('Hello from B!')
+});
+
+// will match anything with an a in the route name:
+app.get(/a/, function(req, res) {
+    res.send('/a/')
+})
+
+// will match butterfly, dragonfly; but not butterflyman, dragonfly man, and so on
+app.get(/.*fly$/, function(req, res) {
+    res.send('/.*fly$/')
+})
 
 app.get('/s', function (req, res) {
     res.send('Hello World!111111sss');
@@ -15,6 +34,7 @@ app.get('/s', function (req, res) {
 var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
-
+    var __dirname = '1';
+    console.info(__dirname);
     console.log('Example app listening at http://%s:%s', host, port);
 });
--- 20150911/app.js
+++ 20150911/app.js
@@ -1,8 +1,106 @@
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
+//var ObjectId = require('mongodb').ObjectID;
 var url = 'mongodb://localhost:27017/test';
 MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
-    console.log("Connected correctly to server.");
-    db.close();
+
+    //insertDocument(db, function() {
+    //    db.close();
+    //});
+
+    //removeRestaurants(db, function() {
+    //    db.close();
+    //});
+
+    //dropRestaurants(db, function() {
+    //    db.close();
+    //});
+
+    findRestaurants(db, function() {
+        db.close();
+    });
+
+    //updateRestaurants(db, function() {
+    //    db.close();
+    //});
 });
+
+var removeRestaurants = function(db, callback) {
+    db.collection('restaurants').deleteMany(
+        { },
+        function(err, results) {
+            console.log(results);
+            callback();
+        }
+    );
+};
+
+var updateRestaurants = function(db, callback) {
+    db.collection('restaurants').replaceOne(
+        { "restaurant_id" : "41704620" },
+        {
+            "name" : "Vella 2",
+            "address" : {
+                "coord" : [ -73.9557413, 40.7720266 ],
+                "building" : "1480",
+                "street" : "2 Avenue",
+                "zipcode" : "10075"
+            }
+        },
+        function(err, results) {
+            console.log(results);
+            callback();
+        });
+};
+
+var dropRestaurants = function(db, callback) {
+    db.collection('restaurants').drop( function(err, response) {
+        console.log(response);
+        callback();
+    });
+};
+
+var findRestaurants = function(db, callback) {
+    var cursor =db.collection('restaurants').find( );
+    cursor.each(function(err, doc) {
+        assert.equal(err, null);
+        if (doc != null) {
+            console.dir(doc);
+        } else {
+            console.log("none data");
+            callback(null);
+        }
+    });
+};
+
+var insertDocument = function(db, callback) {
+    db.collection('restaurants').insertOne( {
+        "address" : {
+            "street" : "2 Avenue",
+            "zipcode" : "10075",
+            "building" : "1480",
+            "coord" : [ -73.9557413, 40.7720266 ],
+        },
+        "borough" : "Manhattan",
+        "cuisine" : "Italian",
+        "grades" : [
+            {
+                "date" : new Date("2014-10-01T00:00:00Z"),
+                "grade" : "A",
+                "score" : 11
+            },
+            {
+                "date" : new Date("2014-01-16T00:00:00Z"),
+                "grade" : "B",
+                "score" : 17
+            }
+        ],
+        "name" : "Vella",
+        "restaurant_id" : "41704620"
+    }, function(err, result) {
+        assert.equal(err, null);
+        console.log("Inserted a document into the restaurants collection.");
+        callback(result);
+    });
+};
